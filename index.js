96cc819bd3ceb82c8e9bcb29ab200947175e9b0c
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-01-08 17:11:28 +0100

    new file:   views/404.html 	new file:   views/about.html 	new file:   views/index.html

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = 3000;
+const SUCCESS_CODE = 200;
+const NOT_FOUND = 400;
 
 const server = http.createServer((request, response) => {
   console.log(request.url, request.method);
 
   //   set header content type
-
   response.setHeader("Content-Type", "text/html");
+  //   response.write("<h1>OGUBUIKE EMEJURU</h1>");
+  //   response.end();
 
-  response.write("<h1>OGUBUIKE EMEJURU</h1>");
+  let path = "./views/";
 
-  response.end();
+  switch (request.url) {
+    case "/":
+      path += "index.html";
+      response.statusCode = SUCCESS_CODE;
+      break;
+    case "/about":
+      path += "about.html";
+      response.statusCode = SUCCESS_CODE;
+      break;
+    case "/about-me":
+      path += "about.html";
+      response.statusCode = 301;
+      response.setHeader("Location", "/about");
+      response.end();
+      break;
+    default:
+      path += "404.html";
+      response.statusCode = NOT_FOUND;
+      break;
+  }
+
+  //   send an HTML FILE
+  fs.readFile(path, (error, data) => {
+    if (error) {
+      console.log(error);
+      response.end();
+    } else {
+      // you can use response.write() when doing multiple readings
+      //   response.write(data);
+
+      response.end(data);
+    }
+  });
 });
 
 server.listen(PORT, "localhost", () => {

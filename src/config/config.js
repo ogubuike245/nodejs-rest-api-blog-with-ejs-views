b5c707063aca64af69dcefd841d33a6814ca8f7c
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-04-10 16:57:34 +0100

    test prod

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 const { MONGODB_URI, API_PORT } = process.env;
 
-export const connectToDatabaseAndStartServer = (app) => {
+const connectToDatabaseAndStartServer = (app) => {
   mongoose.set("strictQuery", false);
-  mongoose.connection.on("connected", () => {
-    console.info(`CONNECTED TO DATABASE`);
+  mongoose.connection.on("connected", function () {
+    console.info("CONNECTED TO DATABASE");
 
     // Start the application after database connection is established
 
-    app.listen(API_PORT || 5000, (response) => {
-      console.info(`Server is running on PORT :${API_PORT}`);
+    app.listen(API_PORT || 5000, function (response) {
+      console.info("Server is running on PORT :" + (API_PORT || 5000));
     });
   });
 
-  mongoose.connection.on("error", (error) => {
+  mongoose.connection.on("error", function (error) {
     console.error(error);
   });
 
@@ -21,4 +21,8 @@ export const connectToDatabaseAndStartServer = (app) => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+};
+
+module.exports = {
+  connectToDatabaseAndStartServer: connectToDatabaseAndStartServer,
 };
